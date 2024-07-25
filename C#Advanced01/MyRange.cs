707bad2e29f6b0a9801928968c851dcbe5791e0e
Mohab662 <mohabmbelkan@gmail.com>
2024-07-25 17:39:34 +0300

    All Assignment

---
+++ C#Advanced01/MyRange.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Advanced01
+{
+    internal class MyRange<T> : IComparable<MyRange<T>>
+    {
+        public int Minimum { get; set; }
+        public int Maximum { get; set; }
+        public MyRange(int _min, int _max)
+        {
+            Minimum = _min;
+            Maximum = _max;
+
+        }
+
+        public bool IsInRange(T value)
+        {
+            if (value is int x)
+            {
+                return (x.CompareTo(Minimum) >= 0) && (x.CompareTo(Maximum) <= 0);
+            }
+
+            return false;
+
+        }
+
+        public int Length()
+        {
+            return Maximum - Minimum;
+        }
+
+        public int CompareTo(MyRange<T>? myRange)
+        {
+            int left;
+            int Right;
+            if (myRange is not null)
+            {
+                left = this.Maximum - this.Minimum;
+                Right = myRange.Maximum - myRange.Minimum;
+                return left.CompareTo(Right);
+            }
+
+            return -1;
+            
+        }
+
+
+
+
+
+    }
+}
--- C#Advanced01/Program.cs
+++ C#Advanced01/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace C_Advanced01
+﻿using System;
+
+namespace C_Advanced01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region (1)
+            /*
+                 *(1).The Bubble Sort algorithm has a time complexity of O(n^2) in 
+                its worst and average cases, which makes it inefficient for 
+                large datasets. How we can optimize the Bubble Sort algorithm
+                And implement the code of this optimized bubble sort algorithm
+                */
+            // We Can Break if is no swap will happen and the array is sorted
+            //int[] arr = new int[6];
+            //bool swapped;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    swapped = false;
+            //    for (int j = 0; j < arr.Length - i - 1; j++)
+            //    {
+            //        if (arr[j] > arr[j + 1])
+            //        {
+
+            //            int temp = arr[j];
+            //            arr[j] = arr[j + 1];
+            //            arr[j + 1] = temp;
+            //            swapped = true;
+            //        }
+            //    }
+
+            //    // If no two elements were swapped by inner loop => break
+            //    if (!swapped)
+            //        break;
+            //}
+
+            #endregion
+
+            #region (2)
+
+            MyRange<int> myRange = new MyRange<int> (5,16);
+            MyRange<int> myRange2 = new MyRange<int>(6, 13);
+
+            Console.WriteLine(myRange.IsInRange(17)); // False
+
+            Console.WriteLine(myRange.Length());  // 11
+
+            Console.WriteLine(myRange.CompareTo(myRange2)); // 1 
+
+
+
+            #endregion
         }
     }
 }
